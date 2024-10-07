e6a44b762a5047c7d86d07b67f9ad5f9a3b33a1d
Newton Wamiti <144038101+Wamitinewton@users.noreply.github.com>
2024-10-07 10:02:20 +0300

    Create valid_parenthesis.kt

---
+++ valid_parenthesis.kt
@@ -0,0 +1,30 @@
+class Solution {
+ fun isValid(s: String): Boolean {
+    val stack = Stack<Char>()
+
+    val map = mapOf(
+        '}' to '{',
+        ')' to '(',
+        ']' to '['
+    )
+
+    s.forEach {
+        stack.push(it)
+
+        if (map.containsKey(it)) {
+            if (stack.size < 2) {
+                return false
+            }
+
+            if (stack[stack.size - 2] != map[it]) {
+                return false
+            }
+
+            stack.pop()
+            stack.pop()
+        }
+    }
+
+    return stack.isEmpty()
+}
+}
